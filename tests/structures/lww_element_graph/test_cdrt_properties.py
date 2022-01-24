e3b13d282ff706252fbefa0f47cf1325d0739e00
Piotr Szyma <thompson2908@gmail.com>
2022-01-24 20:29:00 +0100

    fix: drop comments

--- tests/structures/lww_element_graph/test_cdrt_properties.py
+++ tests/structures/lww_element_graph/test_cdrt_properties.py
@@ -1,11 +1,3 @@
-# A state-based CRDT (CvRDT) must...
-
-# Have a partial order to the values.
-# "Monotonically increase" in state, meaning a new state only ever succeeds the current state in the value's ordering.
-# Define a merge function ("least upper bound") which is idempotent and order-independent.
-# TODO: Test partial ordering, monotonically increase.
-# TODO: Implement compare fn?
-
 from lww_element_graph.structures.lww_element_graph import LwwElementGraph
 
 
@@ -29,7 +21,6 @@ def test_merge_is_order_independent():
     assert first_replica.merge(second_replica) == second_replica.merge(first_replica)
 
 
-
 def test_merge_is_idempotent():
     # Arrange.
     first_replica: LwwElementGraph[int] = LwwElementGraph()
@@ -40,7 +31,3 @@ def test_merge_is_idempotent():
 
     # Act & Assert.
     assert first_replica.merge(first_replica) == first_replica
-
-
-
-
