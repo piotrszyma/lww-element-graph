6c1f3aff043e555288b635d28af41e48ae79e26e
Piotr Szyma <thompson2908@gmail.com>
2022-01-25 20:14:44 +0100

    fix: tests for lww element set

--- tests/structures/lww_element_set/test_set_merge.py
+++ tests/structures/lww_element_set/test_set_merge.py
@@ -1,4 +1,4 @@
-from lww_element_graph.structures.lww_element_set import Bias, LwwElementSet
+from lww_element_graph.structures.lww_element_set import LwwElementSet
 
 
 def test_merge():
@@ -20,32 +20,14 @@ def test_merge():
     assert merged_lww.lookup("ghi") is True
 
 
-def test_merge_remove_previously_added():
-    # Arrange.
-    first_lww: LwwElementSet[str] = LwwElementSet()
-    first_lww.add("abc")
-
-    second_lww: LwwElementSet[str] = LwwElementSet()
-    second_lww.add("abc")
-
-    # Last operation was removal.
-    # It should result in lack of this item in merged_lww.
-    first_lww.remove("abc")
-
-    # Act.
-    merged_lww = first_lww.merge(second_lww)
-
-    # Assert.
-    assert merged_lww.lookup("abc") is False
-
-
 def test_later_add_timestamp_takes_precendence():
     # Arrange.
-    first_lww: LwwElementSet[str] = LwwElementSet(bias=Bias.REMOVES)
-    second_lww: LwwElementSet[str] = LwwElementSet(bias=Bias.REMOVES)
+    first_lww: LwwElementSet[str] = LwwElementSet()
+    second_lww: LwwElementSet[str] = LwwElementSet()
 
     first_lww.add("abc")
     first_lww.remove("abc")
+    # Last operation was add. Element should be in merged set.
     second_lww.add("abc")
 
     # Act.
@@ -57,11 +39,12 @@ def test_later_add_timestamp_takes_precendence():
 
 def test_later_remove_timestamp_takes_precendence():
     # Arrange.
-    first_lww: LwwElementSet[str] = LwwElementSet(bias=Bias.REMOVES)
-    second_lww: LwwElementSet[str] = LwwElementSet(bias=Bias.REMOVES)
+    first_lww: LwwElementSet[str] = LwwElementSet()
+    second_lww: LwwElementSet[str] = LwwElementSet()
 
     first_lww.add("abc")
     second_lww.add("abc")
+    # Last operation was remove. Element should not be in merged set.
     second_lww.remove("abc")
 
     # Act.
---
+++ tests/structures/lww_element_set/test_set_merge_bias.py
@@ -0,0 +1,47 @@
+from gc import freeze
+
+from freezegun import freeze_time
+from lww_element_graph.structures.lww_element_set import Bias, LwwElementSet
+
+
+def test_merge_bias_removes():
+    # Arrange.
+    first_lww: LwwElementSet[str] = LwwElementSet(bias=Bias.REMOVES)
+
+    second_lww: LwwElementSet[str] = LwwElementSet(bias=Bias.REMOVES)
+
+    with freeze_time() as frozen_time:
+        second_lww.add("abc")
+
+        frozen_time.tick()
+
+        # Simulate element added to one set and removed from other at one moment.
+        first_lww.add("abc")
+        second_lww.remove("abc")
+
+    # Act.
+    merged_lww = first_lww.merge(second_lww)
+
+    # Assert.
+    assert merged_lww.lookup("abc") is False, "Element should not be present if bias = REMOVES"
+
+def test_merge_bias_adds():
+    # Arrange.
+    first_lww: LwwElementSet[str] = LwwElementSet(bias=Bias.ADDS)
+    second_lww: LwwElementSet[str] = LwwElementSet(bias=Bias.ADDS)
+
+    with freeze_time() as frozen_time:
+        second_lww.add("abc")
+
+        frozen_time.tick()
+
+        # Simulate element added to one set and removed from other at one moment.
+        first_lww.add("abc")
+        second_lww.remove("abc")
+
+    # Act.
+    merged_lww = first_lww.merge(second_lww)
+
+    # Assert.
+    assert merged_lww.lookup("abc") is True, "Element should be present if bias = ADDS"
+
